6648f3c1f7c4d72fe84b52d64dc2c8a898a688a8
William Herry <WilliamHerryChina@Gmail.com>
2013-09-19 16:41:43 +0800

    use handlebars to templating views

--- public/js/views/menuitemdetails.js
+++ public/js/views/menuitemdetails.js
@@ -1,12 +1,13 @@
 var MenuItemDetails = Backbone.View.extend({
+  template: Handlebars.compile(
+    '<div>' +
+    '<h1>{{name}}</h1>' +
+    '<p><span class="label">{{category}}</span></p>' +
+    '<img src="photos/{{imagepath}}" class="img-polaroid" />' +
+    '</div>'
+  ),
   render: function () {
-    var markup = '<div>' +
-    '<h1>' + this.options.name + '</h1>' +
-    '<p><span class="label">' + this.options.category + '</span></p>' +
-    '<img src="photos/' + this.options.imagepath + '" class="img-polaroid" />' +
-    '</div>';
-
-    this.$el.html(markup);
+    this.$el.html(this.template(this.options));
     return this;
   }
 });
