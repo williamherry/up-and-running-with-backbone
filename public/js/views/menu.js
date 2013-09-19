4c9d24865a887c1b5328d3b59762c7a708315636
William Herry <WilliamHerryChina@Gmail.com>
2013-09-19 21:54:30 +0800

    adding and removing models

--- public/js/views/menu.js
+++ public/js/views/menu.js
@@ -8,6 +8,8 @@ var MenuView = Backbone.View.extend({
 
   initialize: function  () {
     this.listenTo(this.collection, "reset", this.render);
+    this.listenTo(this.collection, "add", this.render);
+    this.listenTo(this.collection, "remove", this.render);
   },
 
   render: function () {
