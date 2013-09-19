d62a69969c2fe1db8c115776a6f84f168f6eb88e
William Herry <WilliamHerryChina@Gmail.com>
2013-09-19 22:55:31 +0800

    deleting data

--- public/js/views/menuitemdetails.js
+++ public/js/views/menuitemdetails.js
@@ -4,15 +4,31 @@ var MenuItemDetails = Backbone.View.extend({
     '<h1>{{name}}</h1>' +
     '<p><span class="label">{{category}}</span></p>' +
     '<img src="photos/{{imagepath}}" class="img-polaroid" />' +
-    '</div>'
+    '</div>' +
+    '<p></p>' +
+    '<button type="button" class="btn btn-danger confirm-delete">Delete</button>'
   ),
 
   initialize: function () {
     this.listenTo(this.model, "change", this.render);
   },
 
+  deleteItem: function () {
+    this.model.destroy(
+      {
+        success: function (model) {
+          app.menuItems.remove(model.get('id'));
+          app.navigate("", {trigger: true});
+        }
+      }
+    );
+  },
+
   render: function () {
     this.$el.html(this.template(this.model.attributes));
+    this.delegateEvents({
+      'click .btn-danger': 'deleteItem'
+    });
     return this;
   }
 
